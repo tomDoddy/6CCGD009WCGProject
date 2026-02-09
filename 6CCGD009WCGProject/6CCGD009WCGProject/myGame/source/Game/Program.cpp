5176cc61cfca53e9232f1957a4f7fd4364401a37
tomDoddy <148048961+tomDoddy@users.noreply.github.com>
2026-02-09 12:10:44 +0000

    initial commit

---
+++ 6CCGD009WCGProject/6CCGD009WCGProject/myGame/source/Game/Program.cpp
@@ -0,0 +1,37 @@
+﻿#include <memory>
+#include "GameException.h"
+#include "Game.h"
+
+
+//memory leak detection
+#if defined(DEBUG) || defined(_DEBUG)
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#endif
+
+using namespace Library;
+
+int WINAPI WinMain(HINSTANCE instance, HINSTANCE previousInstance, LPSTR commandLine, int showCommand)
+{
+
+	//may remove the code here
+	//if enabled, memory leaks are listed in Visual studio's output panel after the program exits.
+#if defined(DEBUG) | defined(_DEBUG)
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+    std::unique_ptr<Game> game(new Game(instance, L"RenderingClass", L"Game Group Project III", showCommand));
+
+    try
+    {
+        game->Run();
+    }
+    catch (GameException ex)
+    {
+        MessageBox(game->WindowHandle(), ex.whatw().c_str(), game->WindowTitle().c_str(), MB_ABORTRETRYIGNORE);
+    }
+
+	//end of remove here
+    return 0;
+}
