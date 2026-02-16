f0487802b9fae66cd66f3c6bacd92930ac9d1ede
tomDoddy <148048961+tomDoddy@users.noreply.github.com>
2026-02-16 15:49:30 +0000

    library

--- 6CCGD009WCGProject/6CCGD009WCGProject/myGame/source/Library/Game.h
+++ 6CCGD009WCGProject/6CCGD009WCGProject/myGame/source/Library/Game.h
@@ -1,11 +1,10 @@
 #pragma once
 
-#include <windows.h>
-#include <string>
+#include "Common.h"
 #include "GameClock.h"
 #include "GameTime.h"
-
-using namespace Library;
+#include "GameComponent.h"
+#include "ServiceContainer.h"
 
 namespace Library
 {
@@ -13,7 +12,7 @@ namespace Library
     {
     public:
         Game(HINSTANCE instance, const std::wstring& windowClass, const std::wstring& windowTitle, int showCommand);
-        ~Game();
+        virtual ~Game();
 
         HINSTANCE Instance() const;
         HWND WindowHandle() const;
@@ -23,18 +22,32 @@ namespace Library
         int ScreenWidth() const;
         int ScreenHeight() const;
 
+        ID3D11Device1* Direct3DDevice() const;
+        ID3D11DeviceContext1* Direct3DDeviceContext() const;
+        bool DepthBufferEnabled() const;
+        float AspectRatio() const;
+        bool IsFullScreen() const;
+        const D3D11_TEXTURE2D_DESC& BackBufferDesc() const;
+        const D3D11_VIEWPORT& Viewport() const;
+
+		const std::vector<GameComponent*>& Components() const;
+		const ServiceContainer& Services() const;
+
         virtual void Run();
         virtual void Exit();
-        virtual void Initialize();
+        virtual void Initialize();		
         virtual void Update(const GameTime& gameTime);
         virtual void Draw(const GameTime& gameTime);
 
     protected:
         virtual void InitializeWindow();
+		virtual void InitializeDirectX();
 		virtual void Shutdown();
 
         static const UINT DefaultScreenWidth;
         static const UINT DefaultScreenHeight;
+		static const UINT DefaultFrameRate;
+        static const UINT DefaultMultiSamplingCount;
 
         HINSTANCE mInstance;
         std::wstring mWindowClass;
@@ -49,6 +62,26 @@ namespace Library
 
         GameClock mGameClock;
         GameTime mGameTime;
+		std::vector<GameComponent*> mComponents;
+		ServiceContainer mServices;
+
+        D3D_FEATURE_LEVEL mFeatureLevel;
+        ID3D11Device1* mDirect3DDevice;
+        ID3D11DeviceContext1* mDirect3DDeviceContext;
+        IDXGISwapChain1* mSwapChain;
+
+        UINT mFrameRate;
+        bool mIsFullScreen;
+        bool mDepthStencilBufferEnabled;
+        bool mMultiSamplingEnabled;
+        UINT mMultiSamplingCount;
+        UINT mMultiSamplingQualityLevels;
+
+        ID3D11Texture2D* mDepthStencilBuffer;
+        D3D11_TEXTURE2D_DESC mBackBufferDesc;
+        ID3D11RenderTargetView* mRenderTargetView;
+        ID3D11DepthStencilView* mDepthStencilView;
+        D3D11_VIEWPORT mViewport;
 
     private:
         Game(const Game& rhs);
